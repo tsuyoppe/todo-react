48193d949469558e5d0c226910fd1bec73797e5f
Tsuyoshi.m <tsuyoshi.m.it@gmail.com>
2021-04-08 15:18:23 +0900

    React.Fragment省略

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDom from "react-dom";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>Hello!</h1>
       <p>World!</p>
-    </React.Fragment>
+    </>
   );
 };
 

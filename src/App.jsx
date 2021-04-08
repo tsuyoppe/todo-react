a9b4e4a8ea90fdaf2fec6bef1af208d819e529a5
Tsuyoshi.m <tsuyoshi.m.it@gmail.com>
2021-04-08 15:33:34 +0900

    HTMLの書き方

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>元気ですか！</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
